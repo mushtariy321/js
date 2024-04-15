815279cb6b34ca622033951db9713df8f322ec6a
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-04-15 13:57:38 +0300

    done

---
+++ main.js
@@ -0,0 +1,14 @@
+// let textPromp = +prompt('son kriting!')
+
+// let number = +prompt('yana son kriting!')
+
+// console.log(textPromp+number);
+// alert
+
+
+let ism = prompt('Ismingizni kiriting!')
+let familya = prompt('Familyangizni kiriting!')
+let yosh = +prompt('Yoshingizni kiriting!')
+let joy = prompt('Yashash manzilingizni kiriting!')
+let fan = prompt('Sevimli faningizni kiriting!')
+console.log(`foydalanuvchi ismi:${ism} \nfoydalanuvchi familyasi:${familya} \nfoydalanuvchi yoshi:${yosh} \nfoydalanuvchi yashash manzili:${joy} \nfoydalanuvchi sevimli fani:${fan}`);
